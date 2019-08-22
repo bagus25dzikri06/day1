f6752e88e5d1e3a161114362113f89527c0fac3c
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-22 16:13:05 +0700

    The introduction function is done.

--- exercise-9.js
+++ exercise-9.js
@@ -8,4 +8,13 @@ function calculateMultiply(angka1, angka2) {
    return angka1 * angka2
 }
 
+/* Fungsi pemrosesan kalimat */
+function processSentence(nama, usia, alamat, hobi) {
+   var kalimat1 = 'Nama saya ' + nama + ', '
+   var kalimat2 = 'umur saya ' + usia + ', '
+   var kalimat3 = 'alamat saya ' + alamat + ', '
+   var kalimat4 = 'dan saya punya hobby yaitu ' + hobi
+   return kalimat1 + kalimat2 + kalimat3 + kalimat4
+}
+
 

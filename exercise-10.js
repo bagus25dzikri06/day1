8b86340b421cbe2807d3c573572185ffd03f2bcf
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-22 16:23:18 +0700

    The NumberComparison function is done.

---
+++ exercise-10.js
@@ -0,0 +1,10 @@
+// Logic Challenge - Compare the Numbers
+function bandingkanAngka(angka1, angka2) {
+   if (angka1 < angka2) {
+      return true
+   } else if (angka1 > angka2) {
+      return false
+   } else {
+      return -1
+   }
+}

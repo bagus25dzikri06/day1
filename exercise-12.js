0acd8407eb4d3002178988c03c9fda687513b4a8
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-23 21:47:29 +0700

    The Minute Conversion program is done

--- exercise-12.js
+++ exercise-12.js
@@ -1,1 +1,15 @@
-// Logic Controller - Minute Converter 
+// Logic Controller - Minute Converter
+function konversiMenit(menit) {
+   var jam = Math.floor(menit / 60)
+   var menitSisa = menit % 60
+
+   if (menit < 60) {
+      return '0:' + menit
+   } else {
+      if (menitSisa < 10) {
+        return jam + ':0' + menitSisa
+      } else {
+        return jam + ':' + menitSisa
+      }
+   }
+} 

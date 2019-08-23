8e6c584d19581ae44da550361692275e0c70dfbf
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-23 14:15:29 +0700

    The program was just added.

--- exercise-11.js
+++ exercise-11.js
@@ -1,1 +1,8 @@
 /* Balik Kata */
+function balikKata(kata) {
+   var kataTerbalik = ''
+   for (var angka = kata.length; angka >= 0; angka--) {
+      kataTerbalik = kataTerbalik + kata[angka]
+   }
+   return kataTerbalik
+}

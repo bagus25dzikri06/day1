2e090d4663bc918c1ac47a4ada53bb5503427dc2
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-08-23 23:39:46 +0700

    The XO program is perfectly accomplished

--- exercise-13.js
+++ exercise-13.js
@@ -1,1 +1,24 @@
-Program X dan O
+/* Program X dan O */
+function xo(str) {
+   var extra = 'x'
+   var ordinary = 'o'
+   var totalX = 0
+   var totalO = 0
+
+   for (var i = 0; i < str.length; i++) {
+      //Menghitung jumlah huruf x
+      if (str.charAt(i) == extra) {
+         totalX += 1
+      //Menghitung jumlah huruf o
+      } else if (str.charAt(i) == ordinary) {
+         totalO += 1
+      }
+   }
+   
+   //Membandingkan jumlah huruf x dan o dalam suatu string
+   if (totalX == totalO) {
+      return true
+   } else {
+      return false
+   }
+}
